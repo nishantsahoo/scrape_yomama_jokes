4e5c07e4702846400df9e21edd8d36f8548d95de
Nishant Sahoo <nishantsahoo.in@gmail.com>
2017-03-16 23:29:26 +0530

    Initial commit

--- scrape_jokes.py
+++ scrape_jokes.py
@@ -5,7 +5,6 @@ from bs4 import BeautifulSoup
 urlList = ['ugly', 'skinny', 'harrypotter', 'obama', 'crazy', 'scifi', 'anime', 'pokemon', 'stupid', 'political', 'fat']
 urlList.extend(['poor', 'nerd', 'doctorwho', 'dark', 'white', 'misc', 'tall', 'short', 'videogame', 'uplifting', 'old'])
 urlList.extend(['dirty', 'lazy', 'crosseyed', 'chatty', 'math', 'music', 'gameofthrones', 'religious', 'hairy', 'bald'])
-urlList.extend([])
 i = 1
 for each in tqdm(urlList):
     sys.stdout = open('DataSets//Yo-Mama-' + each + '.txt', 'w')
